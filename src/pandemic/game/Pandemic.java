71026a809fe65700dbf3566b339512e86f0a4d99
judovana <judovana@email.cz>
2016-11-18 12:29:25 +0100

    Add Main frame of an aplication
    Signed-off-by: Jiri <jvanek@redhat.com>

--- src/pandemic/game/Pandemic.java
+++ src/pandemic/game/Pandemic.java
@@ -5,6 +5,7 @@
  */
 package pandemic.game;
 
+import javax.swing.JFrame;
 import pandemic.game.board.Logic;
 import pandemic.game.board.Board;
 import pandemic.game.roles.Roles;
@@ -16,16 +17,30 @@ import pandemic.game.roles.Roles;
 public class Pandemic {
 
     /**
-     * This method throws RuntimeException  if no player goes to play.
-     * 
+     * This method throws RuntimeException if no player goes to play.
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        if (args.length == 0){
+        if (args.length == 0) {
             throw new RuntimeException("At least one player is expected!");
         }
-        new Board(new Logic(new Roles())).startGame();
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+        //new Board(new Logic(new Roles())).startGame();
     }
-    
-    
+
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Pandemic");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+        frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+    }
 }
