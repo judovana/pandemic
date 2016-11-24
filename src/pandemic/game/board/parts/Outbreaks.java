75a99948310e879fa18c5ec50abac7e2136a506f
judovana <judovana@email.cz>
2016-11-24 13:07:37 +0100

    implemented outbreask graphics
    Signed-off-by: Jiri <jvanek@redhat.com>

--- src/pandemic/game/board/parts/Outbreaks.java
+++ src/pandemic/game/board/parts/Outbreaks.java
@@ -5,14 +5,59 @@
  */
 package pandemic.game.board.parts;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.util.List;
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
 /**
  *
  * @author Pípa
  */
 public class Outbreaks {
-   
-    private int count;
-    
-    public void addOutbreak(){
+
+    private int count = 1;
+    private final List<Point> centers = new ArrayList<>(8);
+    private static int radius = 20;
+
+    public void addOutbreak() {
+        
+    }
+    public Outbreaks() {
+        URL cfgFile = this.getClass().getResource("/pandemic/data/board/outbreaks");
+        try {
+            load(cfgFile);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private void load(URL u) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(u.openStream(), StandardCharsets.UTF_8))) {
+            while (true) {
+                String s = br.readLine();
+                if (s == null) {
+                    break;
+                }
+                s = s.trim();
+                if (s.startsWith("#")) {
+                    continue;
+                }
+                String[] parts = s.split(",");
+                centers.add(new Point(Integer.valueOf(parts[0]), Integer.valueOf(parts[1])));
+
+            }
+        }
+    }
+
+    public void draw(Graphics2D g) {
+        g.setColor(new Color(count * 30, 125, 125, 125));
+        g.fillOval(centers.get(count).x - radius, centers.get(count).y - radius, radius * 2, radius * 2);
     }
 }
