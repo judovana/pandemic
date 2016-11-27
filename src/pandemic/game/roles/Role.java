d9cb3a87cc9cc65ff66600d7305d3ff7d3b2c9af
judovana <judovana@email.cz>
2016-11-27 14:08:32 +0100

    Added special throw for null city.
    There is strange behavior from time to time that role have no city. Dying sooner then later
    
    Signed-off-by: Jiri <jvanek@redhat.com>

--- src/pandemic/game/roles/Role.java
+++ src/pandemic/game/roles/Role.java
@@ -81,6 +81,9 @@ public abstract class Role {
     }
 
     public City getCity() {
+        if (city == null) {
+            throw new RuntimeException("Null city!");
+        }
         return city;
     }
 
