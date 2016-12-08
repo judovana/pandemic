6ba967c0ded55b17d65c063863e5c0e180fac961
judovana <judovana@email.cz>
2016-12-08 15:39:12 +0100

    Implmeneted enum as desired, with correct color<->value mapping
    Signed-off-by: Jiri <jvanek@redhat.com>

--- src/pandemic/game/board/DiseaseType.java
+++ src/pandemic/game/board/DiseaseType.java
@@ -1,15 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pandemic.game.board;
 
+import java.awt.Color;
+
 /**
- *This class is unused - it apeared to be more practical to use direct colors on implementation level.
- * 
+ * This class is unused - it appeared to be more practical to use direct colors
+ * on implementation level.
+ *
  * @author Pípa
  */
 public enum DiseaseType {
-    PLAGUE, FLUX
+    /*blue*/
+    FLUE,
+    /*yellow*/
+    YELLOW_FEWER,
+    /*black*/
+    BLACK_COUGH,
+    /*red*/
+    RABIES;
+
+    public Color toColor() {
+        switch (this) {
+            case FLUE:
+                return Color.blue;
+            case YELLOW_FEWER:
+                return Color.yellow;
+            case BLACK_COUGH:
+                return Color.black;
+            case RABIES:
+                return Color.red;
+            default:
+                throw new RuntimeException("Unsuported color/disease" + this);
+        }
+    }
+
+    public static DiseaseType fromColor(Color c) {
+        if (c.equals(Color.blue)) {
+            return FLUE;
+        } else if (c.equals(Color.yellow)) {
+            return YELLOW_FEWER;
+        } else if (c.equals(Color.black)) {
+            return BLACK_COUGH;
+        } else if (c.equals(Color.red)) {
+            return RABIES;
+        } else {
+            throw new RuntimeException("Unsuported color/disease" + c);
+        }
+    }
+
+    public static void main(String... args) {
+        System.out.println(BLACK_COUGH.toColor());
+        System.out.println(DiseaseType.fromColor(Color.red));
+    }
 }
