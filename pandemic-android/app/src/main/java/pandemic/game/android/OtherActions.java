078509613ae738271e8c60352715c14c2e0c71f5
Jiri <jvanek@redhat.com>
2016-12-25 20:58:27 +0100

    Implemented general enable/disable contract

--- pandemic-android/app/src/main/java/pandemic/game/android/OtherActions.java
+++ pandemic-android/app/src/main/java/pandemic/game/android/OtherActions.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -67,13 +68,17 @@ public class OtherActions extends Activity {
         CurrentPlayerView cp = new CurrentPlayerView(roles.getCurrentPlayer(), this);
         main.addView(cp);
 
+        List<OtherPlayerView> others = new ArrayList<OtherPlayerView>(7);
         for (Role r : roles.getPlayersInCity(roles.getCurrentPlayer().getCity())) {
             if (r != roles.getCurrentPlayer()) {
-                OtherPlayerView op = new OtherPlayerView(r, roles.getCurrentPlayer(),this);
+                OtherPlayerView op = new OtherPlayerView(r, roles.getCurrentPlayer(), this);
                 main.addView(op);
+                others.add(op);
+                op.setListener(cp);
             }
 
         }
+        cp.setListener(others);
 
     }
 
@@ -105,6 +110,7 @@ public class OtherActions extends Activity {
             b.setEnabled(false);
             return b;
         }
+
         protected Button addButon(String title) {
             Button b = new Button(mContext);
             b.setText(title);
@@ -121,17 +127,116 @@ public class OtherActions extends Activity {
             this.addView(cards);
         }
 
+        public Set<j2a.Color> getSelectedColors() {
+            List<PlayerCard> pc = getSelectedCards();
+            Set<j2a.Color> r = new HashSet<j2a.Color>(pc.size());
+            for (PlayerCard card : pc) {
+                r.add(card.getCity().getColor());
+            }
+            return r;
+        }
+
+        public List<PlayerCard> getSelectedCards() {
+            List<PlayerCard> result = new ArrayList<PlayerCard>(checks.size());
+            for (CheckboxWithCard ch : checks) {
+                if (ch.isChecked()) {
+                    result.add(ch.getCard());
+                }
+            }
+            return result;
+        }
+
+        protected void setCheckListener(CompoundButton.OnCheckedChangeListener ocl) {
+            for (CheckboxWithCard ch : checks) {
+                ch.setOnCheckedChangeListener(ocl);
+            }
+        }
+
+    }
+
+    private class OtherPlayerView extends PlayerView {
+        protected final Role currentPlayer;
+        private final Button giveto;
+        private final Button taketo;
+        private final Button drop;
+
+        public OtherPlayerView(Role thisPlayer, Role currentPlayer, Context parent) {
+            super(thisPlayer, parent);
+            this.currentPlayer = currentPlayer;
+            //Button tab = addButon("*******");
+            setName();
+            //tab.setEnabled(false);
+            giveto = addButon("Give to " + currentPlayer.getName());
+            taketo = addButon("Take from " + currentPlayer.getName());
+            giveto.setEnabled(false);
+            taketo.setEnabled(false);
+            addCards();
+            drop = addDropCardsButon();
+            drop.setEnabled(false);
+        }
+
+        public void setListener(CurrentPlayerView cpv) {
+            setCheckListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (getSelectedCards().size() == 1 && getSelectedCards().get(0).getCity().equals(thisPlayer.getCity())) {
+                        giveto.setEnabled(true);
+                    } else {
+                        giveto.setEnabled(false);
+                    }
+                    if (getSelectedCards().size() >= 1) {
+                        drop.setEnabled(true);
+                    } else {
+                        drop.setEnabled(false);
+                    }
+                }
+            });
+        }
+
+        public void setTake(boolean b) {
+            taketo.setEnabled(b);
+        }
     }
 
     private class CurrentPlayerView extends PlayerView {
+
+        private final Button drop;
+        private final Button invent;
+
+        public void setListener(final List<OtherPlayerView> others) {
+            setCheckListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (getSelectedCards().size() == 5 && getSelectedColors().size() == 1) {
+                        invent.setEnabled(true);
+                    } else {
+                        invent.setEnabled(false);
+                    }
+                    if (getSelectedCards().size() >= 1) {
+                        drop.setEnabled(true);
+                    } else {
+                        drop.setEnabled(false);
+                    }
+                    if (getSelectedCards().size() == 1 && getSelectedCards().get(0).getCity().equals(thisPlayer.getCity())) {
+                        for (OtherPlayerView oo : others) {
+                            oo.setTake(true);
+                        }
+                    } else {
+                        for (OtherPlayerView oo : others) {
+                            oo.setTake(false);
+                        }
+                    }
+                }
+            });
+        }
 
         public CurrentPlayerView(final Role thisPlayer, Context parent) {
             super(thisPlayer, parent);
             setName();
             final Button bs = addButon("Build station");
-            if (thisPlayer.getCity().haveStation()){
+            if (thisPlayer.getCity().haveStation()) {
                 bs.setEnabled(false);
-            }else{
+            } else {
                 bs.setEnabled(true);
             }
             bs.setOnClickListener(new OnClickListener() {
@@ -141,7 +246,7 @@ public class OtherActions extends Activity {
                     bs.setEnabled(false);
                 }
             });
-            final Button cureDisease=addButon(CD);
+            final Button cureDisease = addButon(CD);
             tuneCureButton(roles, cureDisease);
             cureDisease.setOnClickListener(
                     new OnClickListener() {
@@ -154,7 +259,7 @@ public class OtherActions extends Activity {
                                 colors.add(cube.getColor());
                             }
                             if (colors.size() == 1) {
-                                j2a.Color cc =  cubes.get(0).getColor();
+                                j2a.Color cc = cubes.get(0).getColor();
                                 if (Drugs.self.isCured(cc)) {
                                     int inLength = cubes.size();
                                     for (int i = 0; i < inLength; i++) {
@@ -214,28 +319,15 @@ public class OtherActions extends Activity {
                     }
             );
 
-            addButon("Invent Cure");
+            invent = addButon("Invent Cure");
+            invent.setEnabled(false);
             addCards();
-            addDropCardsButon();
+            drop = addDropCardsButon();
+            drop.setEnabled(false);
 
         }
     }
 
-    private class OtherPlayerView extends PlayerView {
-        protected final Role currentPlayer;
-
-        public OtherPlayerView(Role thisPlayer, Role currentPlayer, Context parent) {
-            super(thisPlayer, parent);
-            this.currentPlayer = currentPlayer;
-            Button tab = addButon("*******");
-            setName();
-            tab.setEnabled(false);
-            addButon("Give to " + currentPlayer.getName());
-            addButon("Take from " + currentPlayer.getName());
-            addCards();
-            addDropCardsButon();
-        }
-    }
 
     private static class CheckboxWithCard extends CheckBox {
 
