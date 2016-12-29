75e24a30719b468e5345d0702a6e58c120ee2e84
Jiri <jvanek@redhat.com>
2016-12-29 21:30:39 +0100

    Fixed bug when infectioun clould grow over 6

--- pandemic-lib/src/pandemic/game/board/parts/InfecetionRate.java
+++ pandemic-lib/src/pandemic/game/board/parts/InfecetionRate.java
@@ -83,6 +83,9 @@ public class InfecetionRate {
 
     public void addInfectionRate() {
         count++;
+        if (count>=values.size()){
+            count = values.size()-1;
+        }
     }
 
 
